00802f9489d3f524596096d86c169ea2c725c4c6
ZubyApp <109441625+ZubyApp@users.noreply.github.com>
2022-08-15 12:56:49 +0100

    Update HomeController.php
    In preparation for deployment

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,33 +4,21 @@ declare(strict_types = 1);
 
 namespace App\Controllers;
 
-use App\App;
-use App\Container;
-use App\Services\InvoiceService;
-use App\View;
+use App\Attributes\Get;
+use App\Attributes\Route;
+use App\Enums\HttpMethod;
+use Twig\Environment AS Twig;
 
 class HomeController
 {
-    public function __construct(private InvoiceService $invoiceService)
+    public function __construct(private Twig $twig)
     {
     }
 
-    public function index(): View
+    #[Get('/')]
+    #[Route('/home', HttpMethod::Head)]
+    public function index()
     {
-        $this->invoiceService->process([], 25);
-
-        return View::make('index');
-    }
-
-    public function saveFile()
-    {
-        $filePath = \STORAGE_PATH . '/' . $_FILES['transactions']['name'];
-        $filePath1 = \STORAGE_PATH . '/' . $_FILES['transactions1']['name'];
-
-        \move_uploaded_file($_FILES['transactions']['tmp_name'], $filePath);
-        \move_uploaded_file($_FILES['transactions1']['tmp_name'], $filePath1);
-
-        \header("Location: /");
-        exit;
+        return $this->twig->render('index.twig');
     }
 }
