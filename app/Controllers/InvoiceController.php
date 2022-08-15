926b56f5e1a63a4cd156f9dab8f52fdd20a652a2
ZubyApp <109441625+ZubyApp@users.noreply.github.com>
2022-08-15 13:01:54 +0100

    Add files via upload
    Adjusted for deployment

---
+++ app/Controllers/InvoiceController.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Controllers;
+
+use App\Attributes\Get;
+use App\Services\InvoiceService;
+use Twig\Environment AS Twig;
+
+class InvoiceController
+{
+    public function __construct(private Twig $twig, private InvoiceService $invoiceService)
+    {
+        
+    }
+
+    #[Get('/invoices')]
+    public function index(): String
+    {        
+        return $this->twig->render(
+            'invoices/index.twig',
+            ['invoices' => $this->invoiceService->getPaidInvoices()]
+        );
+    }
+}
