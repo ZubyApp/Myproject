5af1c8f096f7d58d9fd885ab22bb8ae010918f1d
ZubyApp <109441625+ZubyApp@users.noreply.github.com>
2022-08-15 13:27:28 +0100

    Add files via upload

---
+++ app/Enums/InvoiceStatus.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Enums;
+
+use App\Enums\Color;
+
+enum InvoiceStatus
+{
+    case Pending = 0;
+    case Paid    = 1;
+    case Void    = 2;
+    case Failed  = 3;
+
+    public function toString(): string
+    {
+        return match($this) {
+            self::Paid =>   'Paid',
+            self::Failed => 'Declined',
+            self::Void =>   'Void',
+            default =>      'Pending',
+        };
+    }
+}
